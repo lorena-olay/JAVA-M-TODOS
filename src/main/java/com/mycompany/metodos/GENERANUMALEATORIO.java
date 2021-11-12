d4a15aa8ce6b98c3d87690b42c746a340b5f51f4
lorena_olay <olaylorena2002@gmail.com>
2021-11-12 14:11:46 +0100

    he entendido que es un juego y he creado la condicional para q el usuario juegue a comparar su numero con el de la máquina

--- src/main/java/com/mycompany/metodos/GENERANUMALEATORIO.java
+++ src/main/java/com/mycompany/metodos/GENERANUMALEATORIO.java
@@ -14,14 +14,14 @@ import java.util.Random;
  * @author lorena
  */
 public class GENERANUMALEATORIO {
-
+    //método para pedir un número entero y controla que si es un número entero
     public static int pedirNumeroEntero() {
         Scanner sc = new Scanner(System.in);
         int numero = 0;
         boolean seguir = true;
         do {
             try {
-                JOptionPane.showMessageDialog(null, "Escribe el primer número entero");
+                JOptionPane.showMessageDialog(null, "Escribe el número entero");
                 numero = sc.nextInt();
                 seguir = false;
             } catch (InputMismatchException ime) {
@@ -32,38 +32,26 @@ public class GENERANUMALEATORIO {
         return numero;
 
     }
-
-    public static int pedirNumeroEntero1() {
-        Scanner sc = new Scanner(System.in);
-        int numero1 = 0;
-        boolean seguir = true;
-        do {
-            try {
-                JOptionPane.showMessageDialog(null, "Escribe el segundo número entero");
-                numero1 = sc.nextInt();
-                seguir = false;
-            } catch (InputMismatchException ime) {
-                JOptionPane.showMessageDialog(null, "eso no es un número entero");
-                sc.nextLine();
-            }
-        } while (seguir);
-        return numero1;
-
-    }
-
+    //método para número aleatorio entre 2 números, el primero tiene que ser mayor que el segundo
     public static int numeroAleatorioEntre(int numero, int numero1) {
         Random numAleatorio = new Random();
         int numeroAleatorio = numAleatorio.nextInt(numero - numero1 + 1) + numero1;
-        System.out.println("Numero aleatorio " + numeroAleatorio);
+        //System.out.println("Numero aleatorio " + numeroAleatorio);
         return numeroAleatorio;
     }
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        numeroAleatorioEntre(pedirNumeroEntero(), pedirNumeroEntero1());
+        //condicional para saber si el número aleatorio generado por la máquina 
+        //es igual que lo que introduce el usuario
+        if(numeroAleatorioEntre(100, 0)==pedirNumeroEntero()){
+            System.out.println("HAS ACERTADO!");
+        }else{
+            System.out.println("PERDISTE");
+        }
     }
 
 }
